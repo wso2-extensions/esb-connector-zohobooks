d5a8ef0d3d84224efce0d856b5e83a2803a17f4c
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-07-20 11:27:20 +0530

    To release the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/zohobooks/ZohoBooksConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/zohobooks/ZohoBooksConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class ZohoBooksConnectorIntegrationTest extends ConnectorIntegrationTestB
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("zohobooks-connector-1.0.1");
+        init("zohobooks-connector-1.0.2");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
